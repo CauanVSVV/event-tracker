668b03570f1385d2c689363627fbb644ac222b5b
Cauan VSVV <cauan.valadares.svv@gmail.com>
2025-06-12 20:36:06 -0300

    feat: implement event filtering system with Recoil state management

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,5 +1,6 @@
 import { atom } from "recoil";
 import { IEvento } from "../interfaces/IEvento";
+import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos";
 
 
 export const listaDeEventosState = atom<IEvento[]>({
@@ -18,4 +19,9 @@ export const listaDeEventosState = atom<IEvento[]>({
         "completo": false,
         "id": 1642342959
     }]
+})
+
+export const filtroDeEventosState = atom<IFiltroDeEventos>({
+    key:"filtroDeEventos",
+    default:{}
 })
